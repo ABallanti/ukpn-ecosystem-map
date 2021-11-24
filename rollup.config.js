1d4edf30a7261405cdf9eb9566fbfecd25127634
Giles Dring <giles@dringtech.com>
2021-11-24 22:08:49 +0000

    Optimising css

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import cssnano from 'cssnano';
 import livereload from 'rollup-plugin-livereload';
 import postcss from 'rollup-plugin-postcss';
 import serve from 'rollup-plugin-serve';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
+import autoprefixer from 'autoprefixer';
 
 const dev = process.env.NODE_ENV === 'development';
 
@@ -16,23 +18,34 @@ export default {
     postcss({
       inject: false,
       extract: 'style.css',
+      plugins: [
+        autoprefixer(),
+        cssnano({
+          preset: 'default',
+        }),
+      ],
     }),
     babel({ babelHelpers: 'bundled' }),
-    dev && serve({
-      contentBase: 'docs',
-      open: true,
-    }),
-    dev && livereload({
-      watch: 'docs',
-      delay: 1000,
-    }),
+    dev &&
+      serve({
+        contentBase: 'docs',
+        open: true,
+      }),
+    dev &&
+      livereload({
+        watch: 'docs',
+        delay: 1000,
+      }),
   ],
-  output: [{
-    file: 'docs/bundle.js',
-    format: 'iife',
-  }, {
-    file: 'docs/bundle.min.js',
-    format: 'iife',
-    plugins: [terser()],
-  }]
+  output: [
+    {
+      file: 'docs/bundle.js',
+      format: 'iife',
+    },
+    {
+      file: 'docs/bundle.min.js',
+      format: 'iife',
+      plugins: [terser()],
+    },
+  ],
 };
