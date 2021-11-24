3df2aa1340473d8120112b681e3a340196b1f772
Giles Dring <giles@dringtech.com>
2021-11-24 21:37:25 +0000

    Add dev server

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import postcss from 'rollup-plugin-postcss';
+import serve from 'rollup-plugin-serve';
 import { terser } from "rollup-plugin-terser";
-import postcss from 'rollup-plugin-postcss'
+
+const dev = process.env.NODE_ENV === 'development';
 
 export default {
   input: 'src/main.js',
@@ -14,6 +18,14 @@ export default {
       extract: 'style.css',
     }),
     babel({ babelHelpers: 'bundled' }),
+    dev && serve({
+      contentBase: 'docs',
+      open: true,
+    }),
+    dev && livereload({
+      watch: 'docs',
+      delay: 1000,
+    }),
   ],
   output: [{
     file: 'docs/bundle.js',
