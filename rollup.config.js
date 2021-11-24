421a1f9f8aa9b09ce6c1a4e948b800c7347b8048
Giles Dring <giles@dringtech.com>
2021-11-24 20:28:39 +0000

    Add terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,21 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/main.js',
   plugins: [
     resolve(),
     commonjs(),
-    babel({ babelHelpers: 'bundled' })
+    babel({ babelHelpers: 'bundled' }),
   ],
-  output: {
+  output: [{
     file: 'docs/bundle.js',
     format: 'iife'
-  }
+  }, {
+    file: 'docs/bundle.min.js',
+    format: 'iife',
+    plugins: [terser()],
+  }]
 };
